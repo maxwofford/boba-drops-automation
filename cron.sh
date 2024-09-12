72b53d617c60d63333b5b8327ac6ee40e328e334
Max Wofford <max@maxwofford.com>
2024-09-12 13:10:16 -0400

    Include path export in cron script

--- cron.sh
+++ cron.sh
@@ -3,6 +3,8 @@
 # Define a lock file
 LOCKFILE="/tmp/boba-drops-automation.lock"
 
+echo "Starting at $(date '+%d/%m/%Y %H:%M:%S')"
+
 # Check if lock file exists
 if [ -e "$LOCKFILE" ]; then
     echo "Script is already running."
@@ -18,9 +20,8 @@ trap 'rm -f "$LOCKFILE"' EXIT
 # Script logic goes here
 echo "Running script..."
 
-bun install
-bun run screenshot.js
-bun run waybackmachine.js
+export PATH=$PATH:$HOME/.bun/bin
+bun install && bun run screenshot.js && bun run waybackmachine.js
 
 # Script completes
 echo "Script finished."
